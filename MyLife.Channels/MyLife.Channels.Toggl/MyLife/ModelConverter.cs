ea92d87efe56a7c43b242e2e1623526507c9890d
LazyTarget <peter.aslund@hotmail.com>
2015-02-15 01:13:44 +0100

    Events and IEventChannel

---
+++ MyLife.Channels/MyLife.Channels.Toggl/MyLife/ModelConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MyLife.Models;
+using Toggl;
+
+namespace MyLife.Channels.Toggl
+{
+    public static class ModelConverter
+    {
+        public static IEvent ToEvent(TimeEntry obj)
+        {
+            var res = new Event
+            {
+                ID = obj.Id.ToString(),
+                Text = obj.Description,
+            };
+
+            DateTime tmp;
+            if (DateTime.TryParse(obj.Start, out tmp))
+                res.StartTime = tmp;
+            if (DateTime.TryParse(obj.Stop, out tmp))
+                res.EndTime = tmp;
+            return res;
+        }
+
+    }
+}
---
+++ MyLife.Channels/MyLife.Channels.Toggl/MyLife/TogglClient.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyLife.Core;
+using MyLife.Models;
+
+namespace MyLife.Channels.Toggl
+{
+    public class TogglClient : IEventChannel
+    {
+        private readonly global::Toggl.Toggl toggl;
+
+        
+        public TogglClient(string key)
+        {
+            toggl = new global::Toggl.Toggl(key);
+        }
+
+
+        public async Task<IEnumerable<IEvent>> GetEvents()
+        {
+            var list = await toggl.TimeEntry.List();
+            var events = list.Select(ModelConverter.ToEvent);
+            return events;
+        }
+
+    }
+}
