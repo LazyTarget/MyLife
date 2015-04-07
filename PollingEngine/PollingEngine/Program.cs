4d89d22b393d8e27baf723525a088e20e699b16b
LazyTarget <peter.aslund@hotmail.com>
2015-04-08 00:28:32 +0200

    Added simple commands

--- PollingEngine/PollingEngine/Program.cs
+++ PollingEngine/PollingEngine/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using PollingEngine.Core;
 
 namespace PollingEngine
@@ -12,8 +13,8 @@ namespace PollingEngine
 
             var manager = new ProgramManager();
             var contexts = new List<PollingContext>();
-            //contexts.Add(new PollingContext(new SteamPoller.SteamPoller(), TimeSpan.FromSeconds(15)));
-            contexts.Add(new PollingContext(new XbmcPoller.XbmcPoller(), TimeSpan.FromSeconds(5)));
+            contexts.Add(new PollingContext(new SteamPoller.SteamPoller(), TimeSpan.FromSeconds(15)));
+            contexts.Add(new PollingContext(new XbmcPoller.XbmcPoller(), TimeSpan.FromSeconds(15)));
 
             manager.Load(contexts);
             manager.Start();
@@ -28,6 +29,64 @@ namespace PollingEngine
                     manager.Exit();
                 else if (input == "end")
                     break;
+                else if (input == "cls")
+                    Console.Clear();
+                else
+                {
+                    var parts = (input ?? "").Trim().Split(' ');
+                    if (parts.Length >= 3)
+                    {
+                        var verb = parts[0];
+                        var subject = parts[1];
+                        var value = parts[2];
+
+
+                        if (verb == "start")
+                        {
+                            if (subject == "program")
+                            {
+                                var ctx = contexts.FirstOrDefault(x => x.Program.GetType().Name == value);
+                                if (ctx != null)
+                                    ctx.State = State.Starting;
+                                else
+                                    Console.WriteLine("Context '{0}' not found", value);
+                            }
+                        }
+                        else if (verb == "stop")
+                        {
+                            if (subject == "program")
+                            {
+                                var ctx = contexts.FirstOrDefault(x => x.Program.GetType().Name == value);
+                                if (ctx != null)
+                                    ctx.State = State.Stopping;
+                                else
+                                    Console.WriteLine("Context '{0}' not found", value);
+                            }
+                        }
+                        else if (verb == "get")
+                        {
+                            if (subject == "program")
+                            {
+                                var ctx = contexts.FirstOrDefault(x => x.Program.GetType().Name.Equals(value, StringComparison.InvariantCultureIgnoreCase));
+                                if (ctx != null)
+                                {
+                                    Console.WriteLine();
+                                    Console.WriteLine("--Program info-- ");
+                                    Console.WriteLine("State: " + ctx.State);
+                                    Console.WriteLine("Interval: " + ctx.Interval);
+                                    Console.WriteLine("Program: " + ctx.Program);
+                                    Console.WriteLine("TimeStarted: " + ctx.TimeStarted);
+                                    Console.WriteLine("TimeStopped: " + ctx.TimeStopped);
+                                    Console.WriteLine();
+                                    Console.WriteLine();
+                                }
+                                else
+                                    Console.WriteLine("Context '{0}' not found", value);
+                            }
+                        }
+
+                    }
+                }
             }
 
             Console.WriteLine("Program will exit after [enter]");
