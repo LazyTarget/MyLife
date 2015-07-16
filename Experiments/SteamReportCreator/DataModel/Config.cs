eaf6c091f741f7c877b510dedd5540273524d44c
Peter Åslund <peter.aslund@hotmail.com>
2015-07-16 19:32:30 +0200

    Filesystem restructuring

--- Experiments/SteamReportCreator/DataModel/Config.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SteamReportCreator.DataModel
-{
-    public static class Config
-    {
-        public const string SteamChannelConnString = "Driver={SQL Server};Server=.;UID=Developer;PWD=123456789;Database=OdbcTest";
-    }
-}
--- MyLife.CoreClient/Config.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MyLife.CoreClient
-{
-    public static class Config
-    {
-        public const string OdbcConnectionString = "Driver={SQL Server};Server=.;UID=Developer;PWD=123456789;Database=OdbcTest";
-
-    }
-}
