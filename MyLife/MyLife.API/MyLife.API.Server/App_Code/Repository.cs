3ab51b90a644aface7a072e82b71a18b2d047dbf
Peter Åslund <peter.aslund@hotmail.com>
2015-11-08 02:35:06 +0100

    Initial API commit

---
+++ MyLife/MyLife.API/MyLife.API.Server/App_Code/Repository.cs
@@ -0,0 +1,13 @@
+﻿using MyLife.Data.Context;
+using ProcessLib.Data;
+
+namespace MyLife.API.Server
+{
+    public class Repository
+    {
+        public MyLifeDataContext MyLife => new MyLifeDataContext();
+
+        public ProcessDataContext Process => new ProcessDataContext();
+
+    }
+}
---
+++ MyLife/MyLife.API/MyLife.API.Server/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace MyLife.API.Server.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
